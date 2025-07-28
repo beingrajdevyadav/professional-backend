76a7d8078f95155616c56bbf95f49fcac6d15aea
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-28 13:10:17 +0530

    cors and cookie-parser installed .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import { configDotenv } from "dotenv";
 import express from "express";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 configDotenv();
 
 
-// db connectionconnectDB();
-connectDB();
+// port
+const port = process.env.PORT || 8000;
 
+// db connectionconnectDB();
+connectDB()
+    .then(
+        () => {
+            app.listen(port, () => {
+                console.log(`Server is serving at http://localhost:${port}`)
+            })
+        }
+    )
+    .catch((err) => {
+        console.log("MongoDb Connection Failed : ", err)
+    })
 
 console.log("Hello, World!");
